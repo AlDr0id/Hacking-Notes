f09605a70c8acf9487372dbd8559f48b9bd95083
Álvaro de la Cruz <adelacruzca@gmail.com>
2019-09-19 11:50:34 +0200

    Add files via upload

--- Scripts/sitemapScrapper.py
+++ Scripts/sitemapScrapper.py
@@ -3,6 +3,10 @@ import sys
 
 doc = ET.parse(sys.argv[1])
 ini = doc.getroot()
+num = 0
 for i in ini:
     url = i[0].text
+    num = num + 1
     print(url)
+
+print("Total: ", num, " resultados")
