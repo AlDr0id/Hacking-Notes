7968414ee3beef4d8d3065c8e6ec0099ee6818db
Álvaro de la Cruz <adelacruzca@gmail.com>
2019-09-19 12:58:14 +0200

    Update scrapperUsersWordpress.py

--- Scripts/scrapperUsersWordpress.py
+++ Scripts/scrapperUsersWordpress.py
@@ -1,8 +1,11 @@
 import json
 import sys
 
-doc = json.loads(open(sys.argv[1],encoding="utf8").read())
-array = doc['@graph'] #########
+#################################################################################################
+# Simple script to obtain the usernames from a json file from /wp-json/wp/v2/users in Wordpress #
+#################################################################################################
+
+doc = json.loads(open(sys.argv[1]).read())
 for elem in array:
         if 'slug' in elem:
             print(str(elem['slug']))
